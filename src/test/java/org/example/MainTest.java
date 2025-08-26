0f380aa1f24ee5949b748ca088d6bb132ea57dec
lazio <ahaffner75@gmail.com>
2025-08-26 17:18:44 +0200

    Test CI - Fehler wurde behoben

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -13,7 +13,7 @@ public class MainTest {
 
     @Test
     void multiply_shouldReturnProduct() {
-        assertEquals(26, Main.multiply(5, 5));
+        assertEquals(25, Main.multiply(5, 5));
     }
 
     @Test
